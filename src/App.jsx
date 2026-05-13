ec3c2736a7ea7cfb3a2429e81070e4f182425392
aame3504 <aame3504@gmail.com>
2026-05-13 19:41:53 +0900

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 const scores = [
-  {이름:"John", 국어: 90, 영어:90, 수학: 80, 과학: 80 },
-  {이름:"Piter",국어: 90, 영어:90, 수학: 80, 과학: 80 },
-  {이름:"Susan",국어: 90, 영어:90, 수학: 80, 과학: 80 },
-  {이름:"Sue",국어: 90, 영어:90, 수학: 80, 과학: 80 },
+  { 이름: "John", 국어: 90, 영어: 90, 수학: 80, 과학: 80 },
+  { 이름: "Piter", 국어: 90, 영어: 90, 수학: 80, 과학: 80 },
+  { 이름: "Susan", 국어: 90, 영어: 90, 수학: 80, 과학: 80 },
+  { 이름: "Sue", 국어: 90, 영어: 90, 수학: 80, 과학: 80 },
 ]
 
-const sources = [0,1,2,3]
-tmp = []
-for(let item of source){
-  tmp.push(item*2)
-}
+const sources = [0, 1, 2, 3]
 
-sources.map(item=>(item*2))
+let tmp = []
+
+for (let item of sources) {
+  tmp.push(item * 2)
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <table>
-      
-      <tr>
-        <th>이름</th>
-        <th>국어</th>
-        <th>영어</th>
-        <th>수학</th>
-        <th>과학</th>
-      </tr>
-      {[0,1,2,3].map(idx => 
-          (
-          <tr>
-            <td>{scores[idx].이름}</td>
-            <td>{scores[idx].국어}</td>
-            <td>{scores[idx].영어}</td>
-            <td>{scores[idx].수학}</td>
-            <td>{scores[idx].과학}</td>
-          </tr>
-          )
-        )
-      }
-      
+    <table border="1">
+      <thead>
+        <tr>
+          {
+            Object.keys(scores[0]).map((key) => (
+              <th key={key}>{key}</th>
+            ))
+          }
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          scores.map((item, idx) => (
+            <tr key={idx}>
+              {
+                Object.values(item).map((value, i) => (
+                  <td key={i}>{value}</td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
   )
 }
